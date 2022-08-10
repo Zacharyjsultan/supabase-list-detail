7ac952eb628bd9d24ee1cf1720d253964a44c4a0
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-08-10 11:54:54 -0700

    anchored

--- render-utils.js
+++ render-utils.js
@@ -1,12 +1,22 @@
 export function renderSpotKind(kindOfSpot) {
     const pTag = document.createElement('p');
     const container = document.createElement('div');
+// figure out anchor
+    const anchor = document.createElement('a');
+
+     //
+    anchor.href = `./details/?id=${kindOfSpot.id}`;
+
     pTag.textContent = kindOfSpot.spot;
     pTag.classList.add('spot-kind');
 
     const img = document.createElement('img');
     img.src = `./assets/${kindOfSpot.spot}.png`;
+   
     container.append(pTag, img);
-
-    return container;
+    
+    //
+    anchor.append(container);
+    //
+    return anchor;
 }
